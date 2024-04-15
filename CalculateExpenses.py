e4efe58aafb0b45e73f10c201eaeefd86d7c7718
prajwal309 <prajwalniraula@gmail.com>
2024-04-14 22:31:03 -0400

    Marianna expense corrected.

--- CalculateExpenses.py
+++ CalculateExpenses.py
@@ -1,6 +1,6 @@
-AshutoshPaysFood = 82.00
-AshutoshPaysFirstRound = 120.51
-DeepsingPaysSecondRound = 75.96
+AshutoshPaidFood = 82.00
+MariannaPaidFirstRound = 120.51
+DeepsingPaidSecondRound = 75.96
 
 #TipFactor = 82.00/74.84
 
@@ -9,21 +9,33 @@ TipFactor = 82.00/69.94
 
 print("The Tip Factor is: ", round(TipFactor,2))
 
-PragyaPays = 9.99*TipFactor+(AshutoshPaysFirstRound+DeepsingPaysSecondRound)/6.0
-MarianaPays  =  (8.99+6.99)*TipFactor+(AshutoshPaysFirstRound+DeepsingPaysSecondRound)/6.0
-PrajwalPays  =  1./8.*(15.99)*TipFactor+(AshutoshPaysFirstRound+DeepsingPaysSecondRound)/6.0
-DeepsingPays  =  7./8.*(15.99)*TipFactor+(AshutoshPaysFirstRound+DeepsingPaysSecondRound)/6.0
-BjoernPays  =  (27.98)/2.*TipFactor+(AshutoshPaysFirstRound+DeepsingPaysSecondRound)/6.0
-AshutoshPays  =  (27.98)/2.*TipFactor+(AshutoshPaysFirstRound+DeepsingPaysSecondRound)/6.0
+PragyaPays = 9.99*TipFactor+(MariannaPaidFirstRound+DeepsingPaidSecondRound)/6.0
+MarianaPays  =  (8.99+6.99)*TipFactor+(MariannaPaidFirstRound+DeepsingPaidSecondRound)/6.0
+PrajwalPays  =  1./8.*(15.99)*TipFactor+(MariannaPaidFirstRound+DeepsingPaidSecondRound)/6.0
+DeepsingPays  =  7./8.*(15.99)*TipFactor+(MariannaPaidFirstRound+DeepsingPaidSecondRound)/6.0
+BjoernPays  =  (27.98)/2.*TipFactor+(MariannaPaidFirstRound+DeepsingPaidSecondRound)/6.0
+AshutoshPays  =  (27.98)/2.*TipFactor+(MariannaPaidFirstRound+DeepsingPaidSecondRound)/6.0
 
 print("Prajwal Pays: ",round(PrajwalPays,2))
 print("Prajwal Pays: 26.81 to Deepsing.")
 print("Prajwal Pays: ",round(PrajwalPays-26.81,2), "to Ashutosh")
-print("Mariana Pays: ", round(MarianaPays, 2), " to Ashutosh")
-print("Pragya Pays: ", round(PragyaPays, 2), " to Ashutosh")
-print("Bjoern Pays: ", round(BjoernPays, 2), " to Ashutosh")
-print("Ashutosh Gets: ", -round(AshutoshPays-AshutoshPaysFood-AshutoshPaysFirstRound, 2))
-print("Deepsing Gets: ", round(DeepsingPays- DeepsingPaysSecondRound,2))
-
+print("*"*5)
+print("Marianna  Gets: ", -round(MarianaPays-MariannaPaidFirstRound, 2))
+print("49.15 from Bjoern")
+print(round(69.03-49.15,2), " from Pragya")
+print("*"*5)
+print("Pragya Pays: ", round(PragyaPays, 2))
+print("Pragya Pays: ", 19.88, "to Marianna" )
+print("Pragya Pays: ", round(44.46-19.88,2), "to Ashutosh" )
+print("*"*5)
+print("Bjoern Pays: ", round(BjoernPays, 2), " to Mariana")
+print("*"*5)
+print("Ashutosh Gets: ", -round(AshutoshPays-AshutoshPaidFood, 2))
+print("24.58 from Pragya")
+print("8.28 from Pragya")
+print("*"*5)
+print("Deepsing Gets: ", -round(DeepsingPays- DeepsingPaidSecondRound,2), " from Prajwal")
+print("*"*30)
 print("Total Expense: ", round(PrajwalPays+MarianaPays+PragyaPays+BjoernPays+AshutoshPays+DeepsingPays,2))
-print("Total Credit: ", round(AshutoshPaysFood+AshutoshPaysFirstRound+DeepsingPaysSecondRound,2))
+print("Total Credit: ", round(AshutoshPaidFood+MariannaPaidFirstRound+DeepsingPaidSecondRound,2))
+print("*"*30)
--- ReadMe.md
+++ ReadMe.md
@@ -2,10 +2,23 @@ The Tip Factor is:  1.17
 Prajwal Pays:  35.09
 Prajwal Pays: 26.81 to Deepsing.
 Prajwal Pays:  8.28 to Ashutosh
-Mariana Pays:  51.48  to Ashutosh
-Pragya Pays:  44.46  to Ashutosh
-Bjoern Pays:  49.15  to Ashutosh
-Ashutosh Gets:  153.36
-Deepsing Gets:  -26.81
+*****
+Marianna  Gets:  69.03
+49.15 from Bjoern
+19.88  from Pragya
+*****
+Pragya Pays:  44.46
+Pragya Pays:  19.88 to Marianna
+Pragya Pays:  24.58 to Ashutosh
+*****
+Bjoern Pays:  49.15  to Mariana
+*****
+Ashutosh Gets:  32.85
+24.58 from Pragya
+8.28 from Pragya
+*****
+Deepsing Gets:  26.81  from Prajwal
+******************************
 Total Expense:  278.47
 Total Credit:  278.47
+******************************
